53cd270792b9fab240218f8f1d5d608e1c763ea5
Rob Moran <rob.moran@arm.com>
2015-01-20 10:41:25 +0000

    Updated build script

--- build.sh
+++ build.sh
@@ -6,13 +6,13 @@ rm -rf build || 0
 yt target ios-simulator
 yt install
 yt up
-yt build -G Xcode
+yt build -G Xcode -- --target yotta-ios
 
 # Open Xcode Project
 open build/ios-simulator/yotta-ios.xcodeproj
 
 # Launch iOS Simulator
-#ios-sim launch build/ios-simulator/source/Debug-iphonesimulator/yotta-ios.app
+#ios-sim launch "build/ios-simulator/source/Debug-iphonesimulator/Yotta iOS.app"
 
 # Deploy to iPhone
-#ios-deploy --debug --bundle build/ios/source/Debug-iphoneos/yotta-ios.app
+#ios-deploy --debug --bundle "build/ios/source/Debug-iphoneos/Yotta iOS.app"
